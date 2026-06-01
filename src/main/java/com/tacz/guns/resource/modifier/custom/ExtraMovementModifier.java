34da5ea478e7b497bb4fa8cdc8f3dd04af515455
MrFoxHD <61489327+MrFoxHD@users.noreply.github.com>
2026-06-02 01:07:19 +0700

    Скорость в ГУИ (Возможно)

--- src/main/java/com/tacz/guns/resource/modifier/custom/ExtraMovementModifier.java
+++ src/main/java/com/tacz/guns/resource/modifier/custom/ExtraMovementModifier.java
@@ -6,6 +6,7 @@ import com.tacz.guns.api.modifier.CacheValue;
 import com.tacz.guns.api.modifier.IAttachmentModifier;
 import com.tacz.guns.api.modifier.JsonProperty;
 import com.tacz.guns.resource.CommonAssetsManager;
+import com.tacz.guns.resource.modifier.AttachmentCacheProperty;
 import com.tacz.guns.resource.pojo.data.gun.GunData;
 import com.tacz.guns.resource.pojo.data.gun.MoveSpeed;
 import net.minecraft.ChatFormatting;
@@ -32,6 +33,41 @@ public class ExtraMovementModifier implements IAttachmentModifier<MoveSpeed, Mov
         ExtraMovementModifier.Data data = CommonAssetsManager.GSON.fromJson(json, ExtraMovementModifier.Data.class);
         MoveSpeed moveSpeed = data.getMoveSpeed();
         return  new ExtraSpeedJsonProperty(moveSpeed);
+    }
+
+    @Override
+    public List<DiagramsData> getPropertyDiagramsData(ItemStack gunItem, GunData gunData, AttachmentCacheProperty cacheProperty) {
+        MoveSpeed cache = cacheProperty.getCache(ID);
+
+        float baseMultiplier = cache.getBaseMultiplier();
+        float aimMultiplier = cache.getAimMultiplier();
+        float reloadMultiplier = cache.getReloadMultiplier();
+
+        double minPercent = -200, maxPercent = 200;
+        double defalutPercent = ((0 - minPercent) / (maxPercent - minPercent));
+
+        double baseModifierPercent = ((baseMultiplier - minPercent) / (maxPercent - minPercent));
+        double aimModifierPercent = ((aimMultiplier - minPercent) / (maxPercent - minPercent));
+        double reloadModifierPercent = ((reloadMultiplier - minPercent) / (maxPercent - minPercent));
+
+        String baseTitleKey = "gui.tacz.gun_refit.property_diagrams.movement_speed";
+        String aimTitleKey = "gui.tacz.gun_refit.property_diagrams.aim_speed";
+        String reloadTitleKey = "gui.tacz.gun_refit.property_diagrams.reload_speed";
+
+        String basePositivelyString = String.format("%.1f%%", baseModifierPercent * 100);
+        String baseNegativelyString = String.format("%.1f%%", baseModifierPercent * 100);
+        String aimPositivelyString = String.format("%.1f%%", aimModifierPercent * 100);
+        String aimNegativelyString = String.format("%.1f%%", aimModifierPercent * 100);
+        String reloadPositivelyString = String.format("%.1f%%", reloadModifierPercent * 100);
+        String reloadNegativelyString = String.format("%.1f%%", reloadModifierPercent * 100);
+
+        String defaultString = String.format("%.1fm", 0.0d);
+
+        return List.of(
+            new DiagramsData(defalutPercent, baseModifierPercent, baseModifierPercent, baseTitleKey, basePositivelyString, baseNegativelyString, defaultString, true),
+            new DiagramsData(defalutPercent, aimModifierPercent, aimModifierPercent, aimTitleKey, aimPositivelyString, aimNegativelyString, defaultString, true),
+            new DiagramsData(defalutPercent, reloadModifierPercent, reloadModifierPercent, reloadTitleKey, reloadPositivelyString, reloadNegativelyString, defaultString, true)
+        );
     }
 
     @Override
