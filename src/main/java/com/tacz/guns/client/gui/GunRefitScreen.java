57d40e296175ba0ddfcd225fe5af7a1836bfcfb9
TRUEC0DER <tcoder090@gmail.com>
2026-06-03 02:00:21 +0300

    Правка рефитскрина

--- src/main/java/com/tacz/guns/client/gui/GunRefitScreen.java
+++ src/main/java/com/tacz/guns/client/gui/GunRefitScreen.java
@@ -136,7 +136,7 @@ public class GunRefitScreen extends Screen {
                     b -> {
                         for (int i = 0; i < 9; i++) {
                             if (player.getInventory().getItem(i).getItem() instanceof IGun) {
-                                player.getInventory().selected = i;
+               ё                 player.getInventory().selected = i;
                                 break;
                             }
                         }
